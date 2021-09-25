da4362ed8b2b351381ee32fc715ce7a8c5aef50d
Pilot <75144446+Mikhail-Khanaev@users.noreply.github.com>
2021-09-25 19:41:50 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
         arrows: false,
         dots: true,
         appendDots: $(".about__navigation"),
-        infinity: true,
+        infinity: false,
         // autoplay: true,
         autoplaySpeed: 6000,
         verticalSwiping: true,
